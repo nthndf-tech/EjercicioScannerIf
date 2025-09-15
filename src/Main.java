6da2ea2c89d7565ce19357591eb12c122c8932a1
Natasha da Silva Fernandes <a25natashadsf@teis.intranet>
2025-09-15 20:50:47 +0200

    ate5

--- src/Main.java
+++ src/Main.java
@@ -31,8 +31,43 @@ public class Main {
         //3. Radar de velocidad: Ingresa la velocidad de un coche. Si es mayor a 120 km/h muestra
         //'Multa', si es menor o igual 'Velocidad correcta'.
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Velocidad del coche");
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Velocidad del coche:");
+//        int velocidad = sc.nextInt();
+//
+//        if (velocidad> 120){
+//            System.out.println("Multa!");
+//        } else {
+//            System.out.println("Velocidad correcta");
+//        }
 
+        //4. Clasificador de animales: Ingresa un animal. Si es 'perro' o 'gato' muestra 'Doméstico'. Si es
+        //'tigre' o 'león' muestra 'Salvaje'. Otro caso: 'No clasificado'.
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Ingresa un animal: ");
+//        String animal = sc.nextLine();
+//
+//        if (animal.equals("perro") || animal.equals("gato")) {
+//            System.out.println("Doméstico.");
+//        } else if (animal.equals("tigre") || animal.equals("león")) {
+//            System.out.println("Selvaje.");
+//        } else{
+//            System.out.println("No clasificado.");
+//        }
+        //5. Entrada a un concierto: Ingresa la edad. Si es menor de 12 no puede entrar, si está entre 12
+        //y 17 necesita adulto, si es 18 o más puede entrar solo
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Edad: ");
+//        int edad = sc.nextInt();
+//
+//        if (edad < 12){
+//            System.out.println("No puede entrar.");
+//        } else if (edad <= 17) {
+//            System.out.println("Necesita adulto.");
+//        } else {
+//            System.out.println("Puede entrar solo.");
+//        }
     }
 }
