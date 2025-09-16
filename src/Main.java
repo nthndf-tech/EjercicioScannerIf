2d865a732ecebabcc69e14b8dadaa241661968b1
Natasha da Silva Fernandes <a25natashadsf@teis.intranet>
2025-09-16 15:46:23 +0200

    falta9

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,8 @@ public class Main {
 //        if (temperatura < 0){
 //            System.out.printf("Hace frío.");
 //        } else if (temperatura < 30){
-//        System.out.printf("Hace calor");
-
-//        }
+//          System.out.printf("Hace calor");
+//          }
 
         //2. Detector de color favorito: Ingresa un color. Si es 'rojo', 'azul' o 'verde', muestra 'Es un color
         //primario', si no, 'Otro color'.
@@ -23,10 +22,9 @@ public class Main {
 //
 //        if (color.equals("rojo") || color.equals("azul") || color.equals("verde")) {
 //            System.out.printf("Es un color primario.");
-//        }    else{
+//        }   else{
 //           System.out.printf("Otro color.");
-//
-//        }
+//           }
 
         //3. Radar de velocidad: Ingresa la velocidad de un coche. Si es mayor a 120 km/h muestra
         //'Multa', si es menor o igual 'Velocidad correcta'.
@@ -39,7 +37,7 @@ public class Main {
 //            System.out.println("Multa!");
 //        } else {
 //            System.out.println("Velocidad correcta");
-//        }
+//          }
 
         //4. Clasificador de animales: Ingresa un animal. Si es 'perro' o 'gato' muestra 'Doméstico'. Si es
         //'tigre' o 'león' muestra 'Salvaje'. Otro caso: 'No clasificado'.
@@ -52,9 +50,9 @@ public class Main {
 //            System.out.println("Doméstico.");
 //        } else if (animal.equals("tigre") || animal.equals("león")) {
 //            System.out.println("Selvaje.");
-//        } else{
+//          } else{
 //            System.out.println("No clasificado.");
-//        }
+//            }
         //5. Entrada a un concierto: Ingresa la edad. Si es menor de 12 no puede entrar, si está entre 12
         //y 17 necesita adulto, si es 18 o más puede entrar solo
 
@@ -66,9 +64,9 @@ public class Main {
 //            System.out.println("No puede entrar.");
 //        } else if (edad <= 17) {
 //            System.out.println("Necesita adulto.");
-//        } else {
+//          } else {
 //            System.out.println("Puede entrar solo.");
-//        }
+//            }
 
         //6. Semáforo: Ingresa un color ('rojo', 'amarillo', 'verde'). Muestra: 'Detente', 'Precaución' o
         //'Avanza'.
@@ -83,6 +81,68 @@ public class Main {
 //            System.out.println("Precaución.");
 //          }  else if (color.equals("verde")) {
 //              System.out.println("Avanza.");
-//               }
+//             }
+        //7. Juego de adivinanza: Ingresa un número secreto y luego una suposición. Indica si acertó, si
+        //el número secreto es mayor o menor.
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Introduce el número secreto: ");
+//        int numSecreto = sc.nextInt();
+//
+//        System.out.println("Introduce el número de suposición: ");
+//        int numSuposicion = sc.nextInt();
+//
+//        if (numSecreto == numSuposicion) {
+//            System.out.println("Acertó!");
+//        } else if (numSecreto > numSuposicion) {
+//            System.out.println("El número es mayor.");
+//            }else {
+//                System.out.println("El número es menor.");
+//            }
+        //8. Comparador de palabras: Ingresa dos palabras. Si son iguales, muestra 'Coinciden'. Si no, 'No coinciden'.
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Ingresa la primera palabra: ");
+//        String palabra1 = sc.nextLine();
+//
+//        System.out.println("Ingresa la segunda palabra: ");
+//        String palabra2 = sc.nextLine();
+//
+//        if (palabra1.equals(palabra2)) {
+//            System.out.println("Coinciden.");
+//        }  else {
+//            System.out.println("No coinciden.");
+//            }
+        //9. Sistema de votación: Ingresa 3 votos (String: 'A', 'B' o 'C'). Muestra quién ganó o si hay
+        //empate.
+
+//        Scanner sc = new Scanner(System.in);
+//        int resultadoA = 0;
+//        int resultadoB = 0;
+//        int resultadoC = 0;
+//
+//        System.out.println("Primer voto: ");
+//        String a = sc.nextLine();
+//
+//        System.out.println("Segundo voto: ");
+//        String b = sc.nextLine();
+//
+//        System.out.println("Tercero voto: ");
+//        String c = sc.nextLine();
+//
+//            if (a.equals(b) && a.equals(c)) {
+//                System.out.println("Empate.");
+//            } else if(
+
+            //10. Operador ternario: Declara una variable entera con una edad. Usa el operador ternario para
+        //guardar en una variable String 'Mayor de edad' o 'Menor de edad' según corresponda, y muéstralo.
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Edad: ");
+//        int edad = sc.nextInt();
+//
+//        String resultado = (edad>=18) ? "Mayor de edad" : "Menor de edad ";
+//        System.out.println(resultado);
     }
 }
